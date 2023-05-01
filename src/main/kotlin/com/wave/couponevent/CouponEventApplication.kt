e3e78eb697026164821376dfdf3d30a5bab5377e
waveofmymind <sjun0913@gmail.com>
2023-05-02 00:37:51 +0900

    Init Project

---
+++ src/main/kotlin/com/wave/couponevent/CouponEventApplication.kt
@@ -0,0 +1,11 @@
+package com.wave.couponevent
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CouponEventApplication
+
+fun main(args: Array<String>) {
+    runApplication<CouponEventApplication>(*args)
+}
---
+++ src/test/kotlin/com/wave/couponevent/CouponEventApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.wave.couponevent
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CouponEventApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
