9eca506cf92eb7fbb38827736992abb5e3217f8e
waveofmymind <sjun0913@gmail.com>
2023-05-26 00:20:55 +0900

    [FEAT] 도메인 모델 추가

---
+++ coupon-core/src/main/kotlin/com/example/couponcore/domain/Coupon.kt
@@ -0,0 +1,28 @@
+package com.example.couponcore.domain
+
+import jakarta.persistence.*
+import java.time.OffsetDateTime
+
+@Entity
+@Table(name = "coupon", schema = "coupon")
+class Coupon(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0,
+    @Column(nullable = false)
+    val title: String,
+
+    @Column(nullable = false)
+    val quantity: Long,
+
+    @Column(nullable = false)
+    val issuedQuantity: Long,
+
+    @Column(nullable = false)
+    val dateIssueStart: OffsetDateTime,
+
+    @Column(nullable = false)
+    val dateIssueEnd: OffsetDateTime,
+
+    @Column(nullable = false)
+    val dateExpire: OffsetDateTime,
+)
