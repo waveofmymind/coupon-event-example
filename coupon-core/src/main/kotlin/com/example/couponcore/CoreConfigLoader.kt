273246779fb6e6d40abc9786e7174041191dc4ec
waveofmymind <sjun0913@gmail.com>
2023-05-26 00:23:01 +0900

    [FEAT] 코어 모듈 설정 로더 클래스 추가

---
+++ coupon-core/src/main/kotlin/com/example/couponcore/CoreConfigLoader.kt
@@ -0,0 +1,14 @@
+package com.example.couponcore
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.cache.annotation.EnableCaching
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.EnableAspectJAutoProxy
+
+@EnableAspectJAutoProxy(exposeProxy = true)
+@EnableCaching
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+class CoreConfigLoader
