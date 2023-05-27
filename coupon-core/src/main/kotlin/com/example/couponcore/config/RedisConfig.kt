aaadbeee807f114f2ff87c2d7885c353b2ef2a7e
waveofmymind <sjun0913@gmail.com>
2023-05-27 18:57:49 +0900

    [FEAT] 레디스 설정 클래스 추가

---
+++ coupon-core/src/main/kotlin/com/example/couponcore/config/RedisConfig.kt
@@ -0,0 +1,52 @@
+package com.example.couponcore.config
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import org.redisson.Redisson
+import org.redisson.api.RedissonClient
+import org.redisson.config.Config
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
+import org.springframework.data.redis.serializer.StringRedisSerializer
+
+@Configuration
+class RedisConfig {
+
+    @Value("\${spring.data.redis.host}")
+    private val host = null
+
+    @Value("\${spring.data.redis.port}")
+    private val port = 0
+
+    @Bean
+    fun redissonClient(): RedissonClient {
+        return Redisson.create(Config().apply { useSingleServer().address = "redis://$host:$port"})
+    }
+
+    @Bean
+    fun redisTemplate(redisConnectionFactory: RedisConnectionFactory) : RedisTemplate<String, Any> {
+        val redisTemplate = RedisTemplate<String,Any>()
+        val serializer =  GenericJackson2JsonRedisSerializer(
+            jacksonObjectMapper()
+                .registerModule(JavaTimeModule())
+                .activateDefaultTyping(
+                    BasicPolymorphicTypeValidator.builder()
+                        .allowIfBaseType(Any::class.java)
+                        .build(),
+                    ObjectMapper.DefaultTyping.EVERYTHING
+                )
+        )
+        redisTemplate.setConnectionFactory(redisConnectionFactory)
+        redisTemplate.keySerializer = StringRedisSerializer()
+        redisTemplate.valueSerializer = serializer
+        redisTemplate.hashKeySerializer = StringRedisSerializer()
+        redisTemplate.hashValueSerializer = serializer
+        return redisTemplate
+    }
+}
